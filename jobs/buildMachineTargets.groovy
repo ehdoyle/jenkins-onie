b89fb049232a7f657776e483a4c75dcee78206c5
Alex Doyle <adoyle@cumulusnetworks.com>
2020-04-20 13:05:15 -0700

    Some progress in config

--- jobs/buildMachineTargets.groovy
+++ jobs/buildMachineTargets.groovy
@@ -1,4 +1,5 @@
 // File is jenkins-onie/jobs/buildMachineTargets.groovy
+// This is jobdsl context
 
 def curFileName="buildMachineTargets.groovy"
 
@@ -9,18 +10,41 @@ def onieBranch="master"
 def stageName="checkout ONIE"
 println "---> ${curFileName} Checking out branch ${onieBranch} from ${onieURL}"
 
-def aJob = job('test job') {
-	label 'test job label'
-	//stage( stageName ) {
+class BuildTarget {
+      def manufacturer
+      def machine
+      def buildEnv
+      def makeTarget
+
+}// BuildTarget 
+def aJob = job('ONIE build') {
+    // any system labeled 'onie' can build.
+	label 'onie'
+	description 'Create ONIE builds'
+	parameters {
+	choiceParam('Build Targets', [ 'all','recovery-iso','demo' ], 'ONIE argument to use')
+	stringParam("buildDebug", "none", "Debug build. Default = none. Options: skipDownload, skipToolBuild ")
+
+
+	}//parameters
+	steps {	
+		shell("git clone --branch ${onieBranch} ${onieURL} ")
+		}
+
 	steps {
 			shell( "pwd ; ls -l ")
-
-			shell("git clone --branch ${onieBranch} ${ONIE_URL} ")
 			shell( "ls -l onie/machine" )
+			//shell( readFileFromWorkspace("find onie/machine -maxdepth 1" ))
+
+
+
+			shell("find onie/machine -maxdepth 1" )
+			
+			//def manulist =  sh( returnStdout: true, script: 'find onie/machine -maxdepth ').trim
+			//println "Manufacturer list: ${manulist}"
 			println( "done!" )
 
 	}//steps
-//}
 }//test job
 
 println "---> ${curFileName} Done."
